edf7a7ad9dca9be98b4e6f7915b9b732a96f3334
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-07-11 16:28:53 +0000

    jumping point

---
+++ src/js/component/card.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+
+
+
+
+
+function Card (){
+ return    <div className="card p-2 align-items-center" style={{ width: '125px', height:'180px' ,margin:'10px', padding:'0'}} id='bigboi'>
+  
+   <div className="card-body" style={{width:'100%' ,margin: '0px'}}>
+     <h1 className="card-title align-items-center" id='numbers'>0</h1>
+     
+    
+   </div>
+ </div>
+
+}
+
+export default Card
